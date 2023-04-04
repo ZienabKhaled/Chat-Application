2add658201848b2a72667f9d471e947aaf8a88b9
Zienab Khaled <zienabkhaled246@gmail.com>
2023-04-04 03:03:37 +0200

    signup logic modified

--- js/signup.js
+++ js/signup.js
@@ -1,27 +1,31 @@
-$(document).ready(function(){
-    $("#signupData").on("submit",function(e){
-        e.preventDefault();
-        //first, get all form data when user signs up
-        let formData = new FormData(this);
-        formData.append("signup","signup");
-        //get $post request in jQuery
-        $.ajax({
-            type:"POST",
-            url:"php/signup.php",
-            data:formData,
-            contentType:false,
-            processData:false,
-            // get response
-            success:function(response){
-                console.log("done");
-                if(response=="success"){
-                    location.href="../users.php";
-                }else{
-                    $("#errors").css("display","block");
-                    $("#errors").html(response);
-                }
-            }
+$(document).ready(function () {
+  $("#signupData").on("submit", function (e) {
+    e.preventDefault();
+    //first, get all form data when user signs up
+    let formData = new FormData(this);
+    formData.append("signup", "signup");
+    // console.log(formData);
+    // get $post request in jQuery
+    $.ajax({
+      type: "POST",
+      url: "php/signup.php",
+      data: formData,
+      contentType: false,
+      processData: false,
+      // get response
+    }).done((res) => {
+      console.log(res);
+    });
+    success: function (response) {
+    console.log("done");
+    console.log(response);
+    if (response == "success") {
+      location.href = "../users.php";
+    } else {
+      $("#errors").css("display", "block");
+      $("#errors").html(response);
+    }
+  },
+  });
+});
 
-        })
-    })
-})
--- php/signup.php
+++ php/signup.php
@@ -1,68 +1,78 @@
 <?php
 include_once("config.php");
+// echo ' <p style="color:red;">hi</p>';
 
-if (isset($POST['signup'])) {
-    if (empty($_POST['fname']) || empty($_POST['lname']) ||empty($_POST['email']) || empty($_POST['password']) || empty($_POST['confirmPassword']) || empty($_POST['image'])) {
+var_dump($_POST);
+if (isset($_POST['signup'])) {
+    if (empty($_POST['fname']) || empty($_POST['lname']) ||empty($_POST['email']) || empty($_POST['password'])
+     || empty($_POST['confirmPassword']) || empty($_FILES['image'])) {
         echo "All inputs are required";
     } else {
         $firstName = mysqli_real_escape_string($conn, $_POST["fname"]);
         $lastName = mysqli_real_escape_string($conn, $_POST["lname"]);
         $email = mysqli_real_escape_string($conn, $_POST["email"]);
 
+    // email validation
+    $emailQuery = "SELECT * FROM `users` WHERE email = '{$email}'";
+    $runEmailQuery = mysqli_query($conn,$emailQuery);
 
-        //email validation
-        $emailQuery = "SELECT * FROM 'users' WHERE email='{$email}'";
-        $runEmailQuery = mysqli_query($conn, $emailQuery);
-        if ($runEmailQuery) {
-            if (mysqli_num_rows($runEmailQuery)> 0) {
-                echo "Email Exists";
-            } else {
-                //check pass
-                if (strlen($_POST["password"]) < 8 || strlen($_POST["confirmPassword"])< 8) {
-                    echo "please use 8 or more characters";
-                } elseif ($_POST["password"] != $_POST["confirmPassword"]) {
-                    echo "please enter the correct password";
-                } else {
-                    //store pass
-                    $password = md5($_POST["password"]);
-                    $image = $_FILES['image'];
-                    $imageName = $image['name'];
-                    $imageSize = $image['size'];
-                    $imageTempName = $image['temp_name'];
-                    $imageType = $image['type'];
+    if($runEmailQuery){
+        if(mysqli_num_rows($runEmailQuery) > 0){
+            echo "Email already exists.";
+        }else{
+            // check password length
+            if(strlen($_POST["password"]) < 8 || strlen($_POST["confirmPassword"]) < 8){
+                echo "Please use 8 or more characters in password";
+            }else if($_POST["password"] != $_POST['confirmPassword']){
+                // match password
+                echo "Password Not Matched";
+            }else{
+                // store password
+                $password = md5($_POST["password"]);
 
-                    // get image extension or type
-                    $explode = explode(".", $imageName);
-                    $lowercase = strtolower(end($explode));
-                    // image extension required
-                    $extension = ["png","jpg","jpeg"];
-                    // if extension not matched
-                    if (in_array($lowercase, $extension) == false) {
-                        echo "This Extension file not allowed,Please choose JPG or PNG.";
-                    } else {
-                        // random number
-                        $random = rand(999999999, 111111111);
-                        $time = time();
-                        // image unique name
-                        $uniqueImageName = $random . $time . $imageName;
+                // profile image
+                $image = $_FILES['image'];
+                $imageName = $image['name'];
+                $imageSize = $image['size'];
+                $imageTempName = $image['tmp_name'];
+                $imageType = $image['type'];
 
-                        // save image
-                        move_uploaded_file($imageTempName, "../assets/" . $uniqueImageName);
+                // get image extension or type
+                $explode = explode(".", $imageName);
+                $lowercase = strtolower(end($explode));
 
-                        //user status
-                        $status="Offline";
-                        $insertQuery = "INSERT INTO `users` (firstName,lastName,email,password,image,status)
-                        VALUES('{$firstName}','{$lastName}','{$email}','{$password}','{$uniqueImageName}','{$status}')";
-                        $runInsertQuery = mysqli_query($conn, $insertQuery);
-                        if(!$runInsertQuery){
-                            echo "Failed while entering data in database";
-                        }else{
-                            echo "success";
-                        }
+                // image extension required
+                $extension = ["png","jpg","jpeg"];
+
+                // if extension not matched
+                if(in_array($lowercase,$extension) == false){
+                    echo "This Extension file not allowed,Please choose JPG or PNG.";
+                }else{
+                    // random number
+                    $random = rand(999999999,111111111);
+                    $time = time();
+                    // image unique name 
+                    $uniqueImageName = $random . $time . $imageName;
+
+                    // save image
+                    move_uploaded_file($imageTempName, "../assets/" . $uniqueImageName);
+
+                    // user status
+                    $status = "Offline";
+
+                    $insertQuery = "INSERT INTO `users` (firstName,lastName,email,password,image,status)
+                    VALUES('{$firstName}','{$lastName}','{$email}','{$password}','{$uniqueImageName}','{$status}')";
+
+                    $runInsertQuery = mysqli_query($conn, $insertQuery);
+                    if(!$runInsertQuery){
+                        echo "Failed while entering data in database";
+                    }else{
+                        echo "success";
                     }
                 }
             }
         }
     }
+    }
 }
-?>
+
